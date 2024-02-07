062fa7e49946ca51970a6dcda96544eee729c1d4
essen <usmanovsn331@gmail.com>
2024-02-07 23:11:16 +0500

    --

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -20,6 +20,6 @@ module.exports = function (req,  res, next) {
         req.user = userData;
         next();
     } catch (e) {
-        // return next(ApiError.UnauthorizedError());
+        return next(ApiError.UnauthorizedError());
     }
 }
